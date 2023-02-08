f4446d2ba71d17a9e2f2f38fde9548bc72d4bc2f
maor <maorshaer87@gmail.com>
2023-02-08 13:04:07 +0200

    complete

---
+++ src/pages/About.jsx
@@ -0,0 +1,17 @@
+
+
+function About() {
+  return (
+    <div>
+        <h1 className="text-6xl mb-4">Github Finder</h1>
+         <p className='mb-4 text-2xl font-light'>
+        A React app to search GitHub profiles and see profile details.<br/>
+        <strong>
+          By <a href='https://github.com/Maor87'> Maor Shaer</a>
+        </strong>
+      </p>
+    </div>
+  )
+}
+
+export default About
